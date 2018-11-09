4e0b8add18458efea0e9246f1794812102d10c79
Omar Mendo <alu0100845396@ull.edu.es>
2018-11-09 12:37:08 +0000

    Creando expextativas - obtener proteinas en kcal - error

--- spec/etiqueta_nutri_spec.rb
+++ spec/etiqueta_nutri_spec.rb
@@ -106,5 +106,8 @@ describe Etiqueta do
         it "Probando método para obtener cantidad de fibra en kilocalorías (Kcal)" do
             expect(@etiqueta.fibra_kcal).to eq(6.4)
         end
+        it "Probando método para obtener cantidad de fibra en kilocalorías (Kcal)" do
+            expect(@etiqueta.proteinas_kcal).to eq(6)
+        end
     end
 end
