5ba67d69edc6da9e007fb5ec70d4147c5930ccc8
Omar Mendo <alu0100845396@ull.edu.es>
2018-11-22 18:06:13 +0000

    Creando fichero pruebas - Individuo

---
+++ spec/individuo_spec.rb
@@ -0,0 +1,15 @@
+# Práctica 8 - Lenguajes y Paradigmas de Programación
+#
+# La etiqueta de información nutricional es la información sobre el aporte energéico (calórico)
+# y contenido de nutrientes que aparece en los envases de los alimentos y bebidas. También se denomina
+#“etiquetado sobre las propiedades nutritivas”.
+#
+# Autor: Omar Mendo Mesa - GH: alu0100845396 / beejeke
+
+# encoding: UTF-8
+
+require "spec_helper"
+
+describe Etiqueta do
+    
+end
