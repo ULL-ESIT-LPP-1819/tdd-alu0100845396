4537a242bea403301bfd5242c3580c702d1d85e3
Omar Mendo <alu0100845396@ull.edu.es>
2018-11-22 18:15:59 +0000

    Creando expectativa para Paciente - El objeto Paciente es una instancia de la clase Paciente

--- spec/individuo_spec.rb
+++ spec/individuo_spec.rb
@@ -12,14 +12,19 @@ require "spec_helper"
 
 describe Etiqueta do
 
-    context "# Expextativas de herencia de la clase Individuo" do
+    context "# Expectativas de herencia de la clase Individuo" do
 
         before :each do
             @individuo = Individuo.new("Marcos")
+
+            @paciente = Paciente.new("Francisco")
         end
 
         it "El objeto Individuo es una instancia de la clase Individuo" do
             expect(@individuo).to be_an_instance_of(Individuo)
+        end
+        it "El objeto Paciente es una instancia de la clase Paciente" do
+            expect(@paciente).to be_an_instance_of(Paciente)
         end
     end
 end
