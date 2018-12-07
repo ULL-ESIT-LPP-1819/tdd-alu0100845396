321288b70dca7022f72dd4347036eec126b29e34
Omar Mendo <alu0100845396@ull.edu.es>
2018-12-07 00:01:56 +0000

    Realizando documentación con YARD

--- lib/prct06/gem/individuo.rb
+++ lib/prct06/gem/individuo.rb
@@ -10,36 +10,44 @@
 
 require "prct06/gem/antrodata"
 
+# Declaración de la clase Individuo
 class Individuo
 
+    # Inclusión del mixin Comparable
     include Comparable
 
     attr_accessor :nombre
 
+    # Método initialize de la clase Individuo
     def initialize(nombre)
         @nombre = nombre
     end
 
 end
 
+# Declaración de cla clase Paciente, que hereda de Individuo
 class Paciente < Individuo
 
     attr_accessor :datos
 
+    # Método initialize de la clase Paciente
     def initialize(nombre, peso, talla, edad, sexo, cintura, cadera)
         @nombre = nombre
         @datos = AntroData.new(peso, talla, edad, sexo, cintura, cadera)
     end
 
+    # Método to_s que muestra los datos y el nombre del paciente
     def to_s
         tmp = "Nombre #{@nombre}\n"
         tmp += @datos.to_s
     end
 
+    # Método <=> del mixin Comparable
     def <=> (other)
         datos.calculate_imc  <=> other.datos.calculate_imc 
     end
 
+    # Método enumerable del mixin Enumerable 
     def enumerable
         datos.calculate_imc
     end
